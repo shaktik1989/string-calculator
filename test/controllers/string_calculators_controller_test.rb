a81dbcfec6342b632d0a16d0c468ddfd1bb08542
shakti <shakti.katare@gmail.com>
2025-01-15 15:00:48 +0530

    add test for custom delimiller

--- test/controllers/string_calculators_controller_test.rb
+++ test/controllers/string_calculators_controller_test.rb
@@ -1,11 +1,31 @@
 require "test_helper"
 
 class StringCalculatorControllerTest < ActionDispatch::IntegrationTest
+  test "should return the 0 in case of empty string" do
+    get string_calculator_add_url, params: { numbers: "" }
+    assert_response :success
+    response = JSON.parse(@response.body)
+    assert_equal 0, response
+  end
+
   # passing a string of comma-separated numbers
   test "should return the sum of numbers" do
     get string_calculator_add_url, params: { numbers: "1,2,3,4" }
     assert_response :success
     response = JSON.parse(@response.body)
     assert_equal 10, response
+  end
+
+  test "should handle new line characters" do 
+    get string_calculator_add_url, params: {numbers: "1\n2,3"}
+    assert_response :success
+    response = JSON.parse(@response.body)
+    assert_equal 6, response
+  end
+
+  test "should return sum with custom delimiter" do
+    get string_calculator_add_url(numbers: "//;\n1;2;3")
+    assert_response :success
+    assert_equal "6", response.body
   end
 end
