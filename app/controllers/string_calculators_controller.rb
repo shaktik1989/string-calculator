96924e4a1f958c262f43ace616497d42f72e86fe
shakti <shakti.katare@gmail.com>
2025-01-15 15:12:34 +0530

    modify code to acomodate custom delimiter

--- app/controllers/string_calculators_controller.rb
+++ app/controllers/string_calculators_controller.rb
@@ -1,7 +1,24 @@
 class StringCalculatorsController < ApplicationController
 	def add
-		numbers = params[:numbers].split(',')
+		if params[:numbers].starts_with?("//")
+			parsed_delimiter = parse_custom_delimiter(params[:numbers])
+			delimiter = parsed_delimiter[0]
+			numbers = parsed_delimiter[1].split(delimiter)
+		else
+			numbers = params[:numbers].gsub("\n", ",").split(',')
+		end	
 		sum = numbers.map(&:to_i).sum
 		render plain: sum
+	end
+
+	private
+
+	def parse_custom_delimiter(input)
+		match = input.match(%r{//(.+?)\n(.*)})
+		if match
+      delimiter = match[1]
+      numbers = match[2]
+      [delimiter, numbers]
+    end
 	end
 end
